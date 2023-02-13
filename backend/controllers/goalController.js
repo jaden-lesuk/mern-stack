d1011614c1901ce28e0f602831542f1968d5ee06
jaden-lesuk <jadenlesuk12@gmail.com>
2023-02-13 08:12:11 +0300

    Added async handler for db communication

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,14 +1,14 @@
 // @desc     Get goals
 // @route    GET /api/goals
 // @access   Private
-const getGoals = (req, res) => {
+const getGoals = async (req, res) => {
     res.status(200).json({message: "No goals yet"});
 }
 
 // @desc     Create a new goal
 // @route    POST /api/goals
 // @access   Private
-const createGoal = (req, res) => {
+const createGoal = async (req, res) => {
     if (!req.body.text){
         res.status(400);
         throw new Error("Goal creation failed, please add text");
@@ -19,14 +19,14 @@ const createGoal = (req, res) => {
 // @desc     Update a goal
 // @route    PUT /api/goals/:id
 // @access   Private
-const updateGoal = (req, res) => {
+const updateGoal = async (req, res) => {
      res.status(200).json({message: `Update goal ${req.params.id}`});
 }
 
 // @desc     Delete a goal
 // @route    DELETE /api/goals/:id
 // @access   Private
-const deleteGoal = (req, res) => {
+const deleteGoal = async (req, res) => {
     res.status(200).json({message: `Delete goal ${req.params.id}`});
 }
 
