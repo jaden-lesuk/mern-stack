d109f5739ef78943ec1ccd0a78cd69e71cadf8e6
jaden-lesuk <jadenlesuk12@gmail.com>
2023-02-13 07:26:42 +0300

    Created a controller for the Goal object

---
+++ backend/controllers/goalController.js
@@ -0,0 +1,34 @@
+// @desc     Get goals
+// @route    GET /api/goals
+// @access   Private
+const getGoals = (req, res) => {
+    res.status(200).json({message: "No goals yet"});
+}
+
+// @desc     Create a new goal
+// @route    POST /api/goals
+// @access   Private
+const createGoal = (req, res) => {
+    res.status(200).json({message: "Goal created"});
+}
+
+// @desc     Update a goal
+// @route    PUT /api/goals/:id
+// @access   Private
+const updateGoal = (req, res) => {
+     res.status(200).json({message: `Update goal ${req.params.id}`});
+}
+
+// @desc     Delete a goal
+// @route    DELETE /api/goals/:id
+// @access   Private
+const deleteGoal = (req, res) => {
+    res.status(200).json({message: `Delete goal ${req.params.id}`});
+}
+
+module.exports = {
+    getGoals,
+    createGoal,
+    updateGoal,
+    deleteGoal,
+}
