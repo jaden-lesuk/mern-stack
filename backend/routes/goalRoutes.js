a14e0b871730477667d2d690fbc9f80f7ab9edca
jaden-lesuk <jadenlesuk12@gmail.com>
2023-02-13 07:30:47 +0300

    Refactored routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -2,12 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { getGoals ,createGoal, updateGoal, deleteGoal} = require("../controllers/goalController");
 
-router.get('/', getGoals);
-
-router.post('/', createGoal);
-
-router.put('/:id', updateGoal);
-
-router.delete('/:id', deleteGoal);
+router.route('/').get(getGoals).post(createGoal);
+router.route('/:id').put(updateGoal).delete(deleteGoal);
 
 module.exports = router;
